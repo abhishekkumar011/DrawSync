c9dd561a2b3789ee34f4a1ecd2a9541ea87c3890
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2026-04-20 00:22:20 +0530

    feat(dashboard): add modular sidebar with org navigation and create action

---
+++ app/(dashboard)/_components/sidebar/list.tsx
@@ -0,0 +1,28 @@
+//This component is used to render the list of current organization
+"use client";
+
+import { Item } from "./item";
+import { useOrganizationList } from "@clerk/nextjs";
+
+export const List = () => {
+  const { userMemberships } = useOrganizationList({
+    userMemberships: {
+      infinite: true,
+    },
+  });
+
+  if (!userMemberships.data?.length) return null;
+
+  return (
+    <ul className="space-y-4">
+      {userMemberships.data?.map((mem) => (
+        <Item
+          key={mem.organization.id}
+          id={mem.organization.id}
+          name={mem.organization.name}
+          imageUrl={mem.organization.imageUrl}
+        />
+      ))}
+    </ul>
+  );
+};
